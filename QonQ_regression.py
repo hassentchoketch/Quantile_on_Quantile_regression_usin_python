ff178abbbee95d9c828767994677f712993242ea
Hacene Tchoketch-kebir <89703595+hassentchoketch@users.noreply.github.com>
2023-03-15 22:18:22 +0000

    up lod new files

--- QonQ_regression.py
+++ QonQ_regression.py
@@ -2,29 +2,57 @@ import os
 import pandas as pd 
 import numpy as np 
 import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt 
+import plotly.graph_objects as go
 
-pd.options.mode.chained_assignment = None
-# import plotly.graph_objects as go
-
-def Q_QR(df,x,y,quarters=None):
+def Q_QR(df= None , x= None, y=None,path= None,quarters=np.arange(0.01,0.9,0.05), plot = True):
   df1=df[[x,y]]
-  results = []
+  slope_results = []
+  intrcept_results = []
   xx = np.linspace(min(df1[x]),max(df1[x]),len(quarters))
   for q in(quarters):
     slopes = []
+    intercepts = []
     for i in range(len(xx)):
-          df1['z']= df1[x].values-xx[i]
+          df1['z']= df1[x]-xx[i]
           r = smf.quantreg(f'{y}~z',df1)
           reg=r.fit(q=q)
           slopes.append(reg.params["z"])
-    results.append(slopes)
-  return results
+          intercepts.append(reg.params['Intercept'])
+    slope_results.append(slopes)
+    intrcept_results.append(intercepts)
+  matrix_slope = np.array(slope_results).T 
+  matrix_intercept = np.array(intrcept_results).T
+
+  if plot:   
+    fig = plt.figure(figsize=(16,8))
+    # fig, ax = plt.subplots(1, 2)
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax2 = fig.add_subplot(112, projection='3d')
+    X, Y = np.meshgrid(quarters, quarters) 
+    surf1 =ax1.plot_surface(X, Y, matrix_slope , cmap= 'viridis',alpha = 0.7 )
+    surf2 =ax2.plot_surface(X, Y, matrix_intercept , cmap= 'viridis',alpha = 0.7 )
+    fig.colorbar(surf1)
+    fig.colorbar(surf2)
+    ax1.set_xlabel(f'{df.columns[0]}_Quartiles')
+    ax1.set_ylabel(f'{df.columns[1]}_Quartiles')
+    ax1.set_zlabel('Slope')
+    ax2.set_xlabel(f'{df.columns[0]}_Quartiles')
+    ax2.set_ylabel(f'{df.columns[1]}_Quartiles')
+    ax2.set_zlabel('Intercept')
+    plt.title(f'Quantile-on-Quantile Process for the {df.columns[0]}  Coefficients',fontdict = {'fontsize': 18,'fontweight': 16})
+    plt.tight_layout()
+    plt.show()
+    fig.savefig(path + '/plot.png')
+    slopes =pd.DataFrame(matrix_slope)
+    intercepts = pd.DataFrame(matrix_intercept)
+    slopes.to_csv(path+'/slopes.csv')
+    intercepts.to_csv(path+'/intercepts.csv')
+  return slopes ,intercepts
 
 
-
+cwd = os.getcwd()
 df = pd.read_csv('data.csv').drop(columns ='Unnamed: 0').set_index('date')
-print(df.info())
+df_slopes,df_interceptes = Q_QR(df = df,x ='NGDP_R_SA_XDC_US',y ='PCPI_IX_US', path = cwd)
 
-results = Q_QR(df,'PCPI_IX_US','NGDP_R_SA_XDC_US',quarters= np.arange(0.01,0.99,0.25).tolist())
-matrix = np.asarray(results).T
-print(results)
+print (cwd)
